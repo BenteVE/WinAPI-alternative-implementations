0a888fc8e948c268f30502030f97379697e8a65d
BenteVE <44496102+BenteVE@users.noreply.github.com>
2024-02-05 20:05:42 +0100

    Reordered code and added better comments

--- MyGetProcAddress.cpp
+++ MyGetProcAddress.cpp
@@ -1,107 +1,40 @@
-/*
-* A similar implementation to GetProcAddress to test if this method works on Wine
-* We try to find the address of a real function through the PEB lists
-*/
-
 #include "MyProcGetAddress.h"
 
-// format typedef struct myStructDefinition{struct info} myStruct, *myStructPointer;
-// (common C idiom to avoid having to write "struct S")
-typedef struct _UNICODE_STRING {
-	USHORT Length;
-	USHORT MaximumLength;
-	PWSTR  Buffer;
-} UNICODE_STRING, * PUNICODE_STRING;
+// A custom implementation of the GetProcAddress() function of the Windows API
+// that uses assembly code to traverse:
+// - the Thread Information Block (TIB)  https://en.wikipedia.org/wiki/Win32_Thread_Information_Block
+// - the Process Environment Block (PEB) https://en.wikipedia.org/wiki/Process_Environment_Block
 
-// PEB loader data structure
-typedef struct _LDR_MODULE
-{
-	LIST_ENTRY      InLoadOrderModuleList;
-	LIST_ENTRY      InMemoryOrderModuleList;
-	LIST_ENTRY      InInitializationOrderModuleList;
-	PVOID           BaseAddress;
-	PVOID           EntryPoint;
-	ULONG           SizeOfImage;
-	UNICODE_STRING  FullDllName;
-	UNICODE_STRING  BaseDllName;
-	ULONG           Flags;
-	SHORT           LoadCount;
-	SHORT           TlsIndex;
-	LIST_ENTRY      HashTableEntry;
-	ULONG           TimeDateStamp;
-} LDR_MODULE, * PLDR_MODULE;
 
-/* Case Insensitive string compare */
-int StringICompareA(const char* strA, LPCSTR strB) {
-	return _stricmp(strA, strB);
-}
-
-/* String compare */
-int StringCompareA(const char* strA, LPCSTR strB) {
-	return strcmp(strA, strB);
-}
-
-/* Unicode to Ascii conversion */
-std::string UnicodeStringToAscii(UNICODE_STRING* unicode) {
-	std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
-	std::wstring wstr(unicode->Buffer, unicode->Length);
-	const std::wstring wide_string = wstr;
-	const std::string utf8_string = converter.to_bytes(wide_string);
-	return utf8_string;
-}
-
-/* Using assembly code to retrieve the pointer to the list of PEB modules */
+// Note: this function only works in x86 because the MSVC compiler doesn't support assembly in x64
 PLIST_ENTRY GetModListPtr()
 {
-	//
-	// IN _PEB_NT:
-	//   PPEB_LDR_DATA                LdrData;               //00C
-	//
-	// IN _PEB_LDR_DATA:
-	//   LIST_ENTRY      InLoadOrderModuleList;              // 0C
-	// 
-	// LIST_ENTRY = Double linked list
-	// typedef struct _LIST_ENTRY {
-	// struct _LIST_ENTRY *Flink;       // offset 00 -> in reality it's a pointer to _LDR_MODULE
-	// struct _LIST_ENTRY *Blink;       // offset 04
-	// } LIST_ENTRY, *PLIST_ENTRY, *RESTRICTED_POINTER PRLIST_ENTRY;
-	//
-	// _LDR_MODULE:
-	// typedef struct _LDR_MODULE {
-	// LIST_ENTRY     InLoadOrderModuleList;               // 00
-	// LIST_ENTRY     InMemoryOrderModuleList;             // 08
-	// LIST_ENTRY     InInitializationOrderModuleList;     // 10
-	// PVOID          BaseAddress;                         // 18
-	// PVOID          EntryPoint;                          // 1C
-	// ULONG          SizeOfImage;                         // 20
-	// UNICODE_STRING FullDllName;                         // 24  
-	// UNICODE_STRING BaseDllName;                         // 28
-	// ...
-	//
-
-	// ReSharper disable once CppDeclaratorNeverUsed
-	PVOID LdrData;
-	PLIST_ENTRY ModList;
-
-	/*
-		Wikipedia on Win32 Thread Information Block: 
-		The TIB can be used to get a lot of information on the process without calling Win32 API. 
-		Examples include emulating GetLastError(), GetVersion(). Through the pointer to the PEB one 
-		can obtain access to the import tables (IAT), process startup arguments, image name, etc. 
-		It is accessed from the FS segment register on 32-bit Windows and GS on 64-bit Windows. 
-
-		fs: [30h] = GS:[60h] => pointer to the Linear address of Process Environment Block (PEB) 
-	*/
+	PVOID LdrData; // Pointer to PPEB_LDR_DATA structure
+	PLIST_ENTRY ModList; // Pointer to a LIST_ENTRY, this is the first  is part of a double linked list of contains pointers toPointer to _LDR_MODULE structure
 
 	__asm
 	{
-		xor eax, eax					//set register to 0 (opcode shorter than: mov eax, 0)
-		xor ecx, ecx
-		mov eax, fs: [30h]				// get pointer to Linear address of Process Environment Block (PEB) 
-		mov ecx, dword ptr[eax + 0Ch]	// IN _PEB_NT: 0Ch offset points to PEB_LDR_DATA LdrData
-		mov[LdrData], ecx				
+		// clear registers (opcode shorter than: mov eax, 0)
 		xor eax, eax
-		mov eax, dword ptr[ecx + 0Ch]	// IN PEB_LDR_DATA: 0Ch offset points to InLoadOrderModuleList
+		xor ecx, ecx
+
+		// Using the TIB to get a pointer to the PEB
+		// The TIB is accessed from the FS segment register on 32-bit Windows and GS on 64-bit Windows
+		// Offset to PEB:  fs: [30h] // GS:[60h]
+		mov eax, fs: [30h]
+
+		// Using the PEB to get a pointer to the PPEB_LDR_DATA structure
+		// typedef struct _PEB			/* win32/win64 */
+		//	 PPEB_LDR_DATA				  /* 00c/018 */
+		mov ecx, dword ptr[eax + 0Ch]
+		mov[LdrData], ecx
+
+		// clear register
+		xor eax, eax
+
+		// Using the PPEB_LDR_DATA to get a pointer to the LIST_ENTRY structure InLoadOrderModuleList
+		// This LIST_ENTRY forms a double linked list and contains a pointer to the first _LDR_MODULE
+		mov eax, dword ptr[ecx + 0Ch]
 		mov[ModList], eax
 	}
 
@@ -123,28 +56,18 @@ PIMAGE_NT_HEADERS GetNtHeader(LPCVOID ModuleBase)
 	return nullptr;
 }
 
+/* Unicode to Ascii conversion */
+std::string UnicodeStringToAscii(UNICODE_STRING* unicode) {
+	std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+	std::wstring wstr(unicode->Buffer, unicode->Length);
+	const std::wstring wide_string = wstr;
+	const std::string utf8_string = converter.to_bytes(wide_string);
+	return utf8_string;
+}
 
 /* Our implementation of the GetModuleHandle function */
 HMODULE MyGetModuleHandle(LPCSTR ModuleName)
 {
-	// _LDR_MODULE:
-	// typedef struct _LDR_MODULE {
-	// LIST_ENTRY     InLoadOrderModuleList;               // 00
-	// LIST_ENTRY     InMemoryOrderModuleList;             // 08
-	// LIST_ENTRY     InInitializationOrderModuleList;     // 10
-	// PVOID          BaseAddress;                         // 18
-	// PVOID          EntryPoint;                          // 1C
-	// ULONG          SizeOfImage;                         // 20
-	// UNICODE_STRING FullDllName;                         // 24  
-	// UNICODE_STRING BaseDllName;                         // 28
-	// ...
-	//
-	// typedef struct _UNICODE_STRING {
-	// USHORT Length;
-	// USHORT MaximumLength;
-	// PWSTR  Buffer;
-	// } UNICODE_STRING, *PUNICODE_STRING;
-
 	const auto ModList = GetModListPtr();
 
 	for (auto ModEntry = ModList->Flink; ModList != ModEntry; ModEntry = ModEntry->Flink)
@@ -156,8 +79,8 @@ HMODULE MyGetModuleHandle(LPCSTR ModuleName)
 			auto TmpModuleName = UnicodeStringToAscii(&LdrMod->BaseDllName);
 			auto TmpModuleNameFull = UnicodeStringToAscii(&LdrMod->FullDllName);
 
-			if (!StringICompareA(TmpModuleName.c_str(), ModuleName) ||
-				!StringICompareA(TmpModuleNameFull.c_str(), ModuleName))
+			if (!_stricmp(TmpModuleName.c_str(), ModuleName) ||
+				!_stricmp(TmpModuleNameFull.c_str(), ModuleName))
 				return static_cast<HMODULE>(LdrMod->BaseAddress); // Module Handle == BaseAddress of that module
 		}
 	}
@@ -165,8 +88,7 @@ HMODULE MyGetModuleHandle(LPCSTR ModuleName)
 	return nullptr;
 }
 
-
-/* Returns the corresponding ordinal number for the specified function name. */
+// Search the Export Address Table of a module for the ordinal number of the specified function name
 DWORD NameToOrdinal(HMODULE ModuleHandle, LPCSTR ProcName)
 {
 	if (ModuleHandle == nullptr)
@@ -199,7 +121,7 @@ DWORD NameToOrdinal(HMODULE ModuleHandle, LPCSTR ProcName)
 	{
 		const auto Name = reinterpret_cast<const char*>(reinterpret_cast<UINT_PTR>(ModuleHandle) + NamesArray[I]);
 
-		if (!StringCompareA(Name, ProcName))
+		if (!strcmp(Name, ProcName))
 		{
 			NameIndex = I;
 			FoundName = TRUE;
@@ -216,10 +138,7 @@ DWORD NameToOrdinal(HMODULE ModuleHandle, LPCSTR ProcName)
 }
 
 
-/*
-*    Our implementation of the GetProcAddress function
-*    in this implementation the argument is the ordinal of an exported function rather than the name
-*/
+// Search the Export Address Table for an export with the given orginal
 FARPROC MyGetProcAddressInternal(HMODULE ModuleHandle, DWORD Ordinal)
 {
 	const auto NtHeader = GetNtHeader(ModuleHandle);
--- MyProcGetAddress.h
+++ MyProcGetAddress.h
@@ -4,12 +4,49 @@
 #include <string>
 #include <codecvt> //for unicode to ascii conversion
 
-typedef struct _UNICODE_STRING UNICODE_STRING;
-typedef struct _LDR_MODULE LDR_MODULE;
+// These structures are only partially available in the official winternl.h header
+// but they can be found on the internet or through reverse engineering
+// https://github.com/wine-mirror/wine/blob/master/include/winternl.h
 
-int StringICompareA(const char* strA, LPCSTR strB);
+typedef struct _UNICODE_STRING {
+	USHORT Length;
+	USHORT MaximumLength;
+	PWSTR  Buffer;
+} UNICODE_STRING, * PUNICODE_STRING;
 
-int StringCompareA(const char* strA, LPCSTR strB);
+
+// PEB loader data structure
+typedef struct _LDR_MODULE
+{
+	// LIST_ENTRY (declared in winnt.h) is a part of a double linked list:
+	// -> *Flink and *Blink point to the next en previous _LDR_MODULE in the list
+	LIST_ENTRY      InLoadOrderModuleList;
+	LIST_ENTRY      InMemoryOrderModuleList;
+	LIST_ENTRY      InInitializationOrderModuleList;
+	PVOID           BaseAddress;
+	PVOID           EntryPoint;
+	ULONG           SizeOfImage;
+	UNICODE_STRING  FullDllName;
+	UNICODE_STRING  BaseDllName;
+	ULONG           Flags;
+	SHORT           LoadCount;
+	SHORT           TlsIndex;
+	LIST_ENTRY      HashTableEntry;
+	ULONG           TimeDateStamp;
+} LDR_MODULE, * PLDR_MODULE;
+
+typedef struct _PEB_LDR_DATA
+{
+	ULONG               Length;
+	BOOLEAN             Initialized;
+	PVOID               SsHandle;
+	LIST_ENTRY          InLoadOrderModuleList;
+	LIST_ENTRY          InMemoryOrderModuleList;
+	LIST_ENTRY          InInitializationOrderModuleList;
+	PVOID               EntryInProgress;
+	BOOLEAN             ShutdownInProgress;
+	HANDLE              ShutdownThreadId;
+} PEB_LDR_DATA, * PPEB_LDR_DATA;
 
 std::string UnicodeStringToAscii(UNICODE_STRING* unicode);
 
